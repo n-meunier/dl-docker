87db388e8a6d99053e8959b05885990b7e9d2794
n-meunier <nicholas.meunier@ascent.ai>
2020-03-10 10:52:12 +0900

    add alias, run container as USER instead of root

--- start_container.sh
+++ start_container.sh
@@ -1,6 +1,19 @@
-DOCKER_IMAGE="dl-docker"
+#!/usr/bin/env bash
 
-docker run -it  --name "my-dl-container" \
+IMAGE_NAME="dl-docker"
+CONTAINER_NAME="${USER}-$IMAGE_NAME"
+
+start_container () {
+    docker run --rm -it --net=host --name $CONTAINER_NAME \
+    -u $(id -u ${USER}):$(id -g ${USER}) \
+    --privileged \
     -v ~/Desktop/dl-docker-dir:/dl-docker-dir \
+    -v /etc/passwd:/etc/passwd:ro \
+    -v /etc/group:/etc/group:ro \
     -w /dl-docker-dir \
-    $DOCKER_IMAGE
+    $IMAGE_NAME
+}
+
+xhost + >> /dev/null
+start_container
+xhost - >> /dev/null
